15ca387742d792da74216c57de12a9bd2f401252
Shubham Gupta <shubham272101@gmail.com>
2024-06-11 21:13:16 +0530

    update Experience component

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,21 +1,28 @@
-import { ExpCard } from "../components/ExpCard"
+import { ExpCard } from "../components/ExpCard";
 
 export const Experience = () => {
-    return <div>
-        <div className="font-mono text-3xl font-bold flex justify-center p-6">
-            Experience
+    return (
+        <div>
+            <div className="font-mono text-3xl font-bold flex justify-center p-6">
+                Experience
+            </div>
+            <div className="px-4 md:px-10 lg:px-20 py-4">
+                <ExpCard 
+                    CmpName={"Pine Labs Pvt. Ltd. "} 
+                    sDate={"06-01-2024"} 
+                    eDate={"05-04-2024"} 
+                    role={"Engineer"} 
+                    desc={
+                        <div className="text-base font-semibold py-4 gap-2">
+                            <p>• Perform Unit testing on API's as well as creating API's</p>
+                            <p>• Performs operational activities as defined by Configuration Management policies and procedures.</p>
+                            <p>• Works to fulfil Configuration Management Service Requests as assigned.</p>
+                            <p>• Engages with strategic projects to ensure Configuration Management are delivered as per requirements within project timeframes.</p>
+                            <p>• Ability to manage multiple projects and clients simultaneously.</p>
+                        </div>
+                    }
+                />
+            </div>
         </div>
-        <div className="px-12 py-4">
-            <ExpCard CmpName={"Pine Labs Pvt. Ltd. "} sDate = {"06-01-2024"} eDate = {"05-04-2024"} role = {"Engineer"} desc={<div className="text-base font-semibold py-4 gap-2">
-                    <p>• Perform Unit testing on API's as well as creating API's</p>
-                    <p>• Performs operational activities as defined by Configuration Management policies
-                    and procedures.</p>
-                    <p>• Works to fulfil Configuration Management Service Requests as assigned.</p>
-                    <p>• Engages with strategic projects to ensure Configuration Management are delivered
-                    as per requirements within project timeframes.</p>
-                    <p>• Ability to manage multiple projects and clients simultaneously.</p>
-                </div>}
-            />
-        </div>
-    </div>
-}
+    );
+};
