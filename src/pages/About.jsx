22f4fbdae77be2b918e2702ba0bd9ff426f51174
Shubham Gupta <shubham272101@gmail.com>
2024-06-11 20:24:28 +0530

    update about component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,16 @@
-    export const About = () => {
-        return <div className="font-mono px-20 ">
+export const About = () => {
+    return (
+        <div className="font-mono px-4 md:px-10 lg:px-20">
             <div className="text-4xl flex justify-center font-bold p-4">
                 About Me
             </div>
 
-            <div className="text-2xl font-bold p-6">
-                Personal Info : 
+            <div className="text-xl md:text-2xl font-bold p-6">
+                Personal Info :
                 <div className="container border-gray-300 border-4 p-4 rounded-3xl mt-2 bg-white">
                     <div className="mb-4 px-4 flex justiffy-center flex-col">
-                        <div className="text-3xl font-semibold mb-2 mt-4">
-                            Shubham Gupta 
+                        <div className="text-2xl md:text-3xl font-semibold mb-2 mt-4">
+                            Shubham Gupta
                         </div>
                         <div className="text-base text-slate-600">
                             Software Engineer
@@ -26,11 +27,11 @@
                     </div>
                 </div>
             </div>
-            <div className="text-2xl font-bold p-6">
-                Education : 
+            <div className="text-xl md:text-2xl font-bold p-6">
+                Education :
                 <div className="container border-gray-300 border-4 p-4 rounded-3xl mt-2 bg-white">
                     <div className="mb-4">
-                        <div className="text-xl font-semibold mb-4 mt-4">
+                        <div className="text-lg md:text-xl font-semibold mb-4 mt-4">
                             - Master's in Computer Application - 7.4 CGPA
                         </div>
                         <div className="text-base font-normal text-slate-600 px-6">
@@ -38,7 +39,7 @@
                         </div>
                     </div>
                     <div className="mb-4 mt-4">
-                        <div className="text-xl font-semibold mb-2">
+                        <div className="text-lg md:text-xl font-semibold mb-2">
                             - Bachelor's in Computer Application - 7.7 CGPA
                         </div>
                         <div className="text-base font-normal text-slate-600 px-6">
@@ -47,10 +48,10 @@
                     </div>
                 </div>
             </div>
-            <div className="text-2xl font-bold p-6">
-                Technical Skills : 
+            <div className="text-xl md:text-2xl font-bold p-6">
+                Technical Skills :
                 <div className="container border-gray-300 border-4 p-4 rounded-3xl mt-2 bg-white">
-                    <div className="grid grid-cols-3 gap-4 text-base text-slate-600 px-12 py-2">
+                    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 text-base text-slate-600 px-6 py-2">
                         <div>
                             Flutter
                         </div>
@@ -100,4 +101,5 @@
                 </div>
             </div>
         </div>
-    }
+    );
+};
