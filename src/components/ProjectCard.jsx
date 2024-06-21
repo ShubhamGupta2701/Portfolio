76e9792ffbd766254b3c9baa08f5abaca3d6bcb8
Shubham Gupta <shubham272101@gmail.com>
2024-06-21 15:37:26 +0530

    update images in projects component and add border to it.

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,7 +1,7 @@
 export const ProjectCard = ({ title, desc, image, isReversed }) => {
     return (
         <div className={`font-mono flex flex-col lg:flex-row ${isReversed ? 'lg:flex-row-reverse' : ''} gap-8 py-8 my-4 border-4 px-8 rounded-3xl bg-white`}>
-            <img src={image} alt={title} className="w-full lg:w-1/3 h-auto rounded-3xl" />
+            <img src={image} alt={title} className="w-full lg:w-1/3 h-auto rounded-3xl border-black border-2" />
             <div className="flex flex-col justify-center lg:w-2/3">
                 <div className="text-3xl font-extrabold py-8">
                     {title}
